1f96f05fa278a89993fd51a7f5a7b4e04a728691
Nadjaroemer <nadja.kuti@gmail.com>
2020-10-21 12:18:37 +0200

    add random colour

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ function famousFunnyNames() {
     var firstName = ["Brad", "Angelina", "George", "Haruki", "Britney", "Elton", "Mother", "Ada", "George", "Barak", "Zack", "Nelson", "Frida","Elvis", "Anne", "Patty"];
     var lastName = ["Clooney", "John", "Obama", "Pitt", "Loveless", "Kahlo", "Bush", "Afron", "Theresa","Smith", "Mandela", "Murakami", "Frank", "Presley", "Clooney", "Spears"];
 
+    var colors =["\x1b[31m", "\x1b[33m", "\x1b[32m", "\x1b[34m", "\x1b[36m"];
+    var reset = "\x1b[0m";
+
     function shuffleNames(i) {
         return i[Math.floor(Math.random() * i.length)];
     }
 
     for (var i = 0; i < 1; i++) {
-        console.log(shuffleNames(firstName) + " " + shuffleNames(lastName));
+        console.log(shuffleNames(colors)+ shuffleNames(firstName) + reset + " " + shuffleNames(colors) + shuffleNames(lastName)+ reset);
     }
 }
+
+
 module.exports = famousFunnyNames();
